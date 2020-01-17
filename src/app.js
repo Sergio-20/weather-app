2b6695f724bc8772b46a92a59dbe6651560aa496
Sergio Gutierrez <nubesgladius20@gmail.com>
2020-01-17 12:07:35 -0800

    forecast and geocode path updated

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const express = require( "express" );
 const hbs = require( "hbs" );
 
 //Custom Modules
-const forecast = require( "./utilities/forecast.js" );
-const geoCode = require( "./utilities/geoCode.js" );
+const forecast = require( "src/utilities/forecast.js" );
+const geoCode = require( "src/utilities/geoCode.js" );
 const log = console.log;
 
 //Define paths for Express config
